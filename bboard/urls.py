ee9de29db25960d0fbb791c4e98ebfd35fc3bc35
Arman <armanospanov212@gmail.com>
2026-01-15 20:39:13 +0500

    first commit

--- bboard/urls.py
+++ bboard/urls.py
@@ -1,9 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdateView, TaskDeleteView
+
+app_name = "tasks"
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls')),  # маршруты ToDo
-    path('', lambda request: redirect('task_list')),  # редирект с корня на список задач
+    path('', TaskListView.as_view(), name='task_list'),
+    path('create/', TaskCreateView.as_view(), name='task_create'),
+    path('<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    path('<int:pk>/edit/', TaskUpdateView.as_view(), name='task_update'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
--- bboard/views.py
+++ bboard/views.py
@@ -1,5 +1,36 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 
-def index(request):
-    return HttpResponse("Привет, бобры!")
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+    ordering = ['-id']
+
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    context_object_name = 'task'
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    template_name = 'tasks/task_form.html'
+    fields = ['title', 'description', 'is_completed']
+    success_url = reverse_lazy('tasks:task_list')  
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'tasks/task_form.html'
+    fields = ['title', 'description', 'is_completed']
+    success_url = reverse_lazy('tasks:task_list') 
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('tasks:task_list')  
